e59ebcd23dec9e461bc57ca025daebed98fa7d7e
NASSRI Hamdi <hnassri@localhost.localdomain>
2021-03-14 12:16:38 +0100

    page de listing des albums faite, version basique

--- frontend/src/pages/album/Album.jsx
+++ frontend/src/pages/album/Album.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const Album = (props) => {
-   return <h1>Album</h1>
+class Album extends Component{
+   constructor(){
+      super();
+      this.state = {
+         albums: []
+      }
+      this.getAlbums();
+   }
 
-   
+   /*async getArtist(id){
+      let nameArtist = await axios.get(`http://127.0.0.1:8000/api/artists/${id}`).then(res => {
+         return res.data.name
+      })
+      
+      return nameArtist
+   }*/
+
+   getAlbums(){
+      axios.get(`http://127.0.0.1:8000/api/albums`).then(res => {
+         return this.setState({
+            albums: res.data["hydra:member"]
+         });
+      });
+   }
+
+   renderAlbum(){
+      return this.state.albums.map((album) => {
+         return (
+            <div key={album}>
+               <Link to={`/album/${album.id}`}>
+                  <h3>{album.name}</h3>
+                  <p><img src={album.coverSmall} alt="img cover" />{album.description}</p>
+               </Link>
+            </div>
+         )
+      })
+   }
+
+   render() {
+      return (
+         <div>
+            <h1>Album</h1>
+            <p>Hello world</p>
+            <div>{this.renderAlbum()}</div>
+         </div>
+      )
+   }
 }
 export default Album;
